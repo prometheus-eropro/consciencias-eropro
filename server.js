df6bbc2c7529ec380ab55a24012adcff80c8fd6d
prometheus-eropro <assistentes.prometheus@gmail.com>
2025-06-30 17:10:53 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const openai = new OpenAI({
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 
 const PORT = process.env.PORT || 10000;
 
@@ -86,9 +86,8 @@ function registrarLog(tipo, mensagem, usuario = 'Sistema') {
 // ====================== API ==========================
 
 // Rota raiz obrigatória para o Render
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'src', 'public', 'index.html'));
 });
 
 // Login com atualização do CSV
