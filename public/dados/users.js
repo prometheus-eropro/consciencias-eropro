1fa4fe302711551df93d07eec03039612429abee
Edgar Rocha Oliveirà <assistentes.prometheus@gmail.com>
2025-07-17 15:00:07 -0300

    Mensagem do commit

--- public/dados/users.js
+++ public/dados/users.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const usuarios = [
  
   {
@@ -14,20 +13,3 @@ const usuarios = [
     "cons": ["agron", "raizes"]
   }
 ]
-=======
-const usuarios = [
- 
-  {
-    "email": "teste@exemplo.com",
-    "celular": "28999999999",
-    "senha": "1234",
-    "cons": ["pontex"]
-  },
-  {
-    "email": "outro@exemplo.com",
-    "celular": "28988888888",
-    "senha": "abcd",
-    "cons": ["agron", "raizes"]
-  }
-]
->>>>>>> fbb25835c563418442fc77a8c932c7bd1fd82e46
