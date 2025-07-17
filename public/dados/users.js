459a3b62150388f9e1edc6788ae0b791c52e5ca2
Edgar Rocha Oliveirà <assistentes.prometheus@gmail.com>
2025-07-16 21:21:15 -0300

    Movendo arquivos para a pasta public

---
+++ public/dados/users.js
@@ -0,0 +1,15 @@
+const usuarios = [
+ 
+  {
+    "email": "teste@exemplo.com",
+    "celular": "28999999999",
+    "senha": "1234",
+    "cons": ["pontex"]
+  },
+  {
+    "email": "outro@exemplo.com",
+    "celular": "28988888888",
+    "senha": "abcd",
+    "cons": ["agron", "raizes"]
+  }
+]
